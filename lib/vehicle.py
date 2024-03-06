135476e984b2155585be185360ff49d8678b35f8
ManassesGikwa <manassesgikwa@gmail.com>
2024-03-06 21:05:45 +0300

    completed Assignment

--- lib/vehicle.py
+++ lib/vehicle.py
@@ -1,2 +1,10 @@
 class Vehicle:
-    pass
+    def __init__(self, wheel_size, wheel_number):
+        self.wheel_size = wheel_size
+        self.wheel_number = wheel_number
+
+    def fill_up_tank(self):
+        return f"filling up!"
+    
+    def go(self):
+        return f"vrrrrrrrooom!"
